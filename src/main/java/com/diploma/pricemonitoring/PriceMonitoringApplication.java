f1f3dbda7213e7f43d1b9c8c281c38bc36470e82
Nazar Sekh <nazar.sekh@gmail.com>
2021-02-23 21:07:11 +0200

    init project

---
+++ src/main/java/com/diploma/pricemonitoring/PriceMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.diploma.pricemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diploma/pricemonitoring/PriceMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diploma.pricemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
